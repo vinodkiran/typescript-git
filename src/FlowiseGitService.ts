48a2c2b19972524bca48f6938754f2daa49f8dbd
Vinod Paidimarry <vinodkiran@outlook.in>
2024-09-19 19:24:04 +0530

    more functionality to GitService

--- src/FlowiseGitService.ts
+++ src/FlowiseGitService.ts
@@ -1,9 +1,10 @@
-import {simpleGit, SimpleGit, SimpleGitOptions} from "simple-git";
+import {ConfigValues, simpleGit, SimpleGit, SimpleGitOptions} from "simple-git";
+import {DiffResult} from "simple-git";
 
 export class FlowiseGitService {
     private gitOptions: Partial<SimpleGitOptions> = {};
     private git: SimpleGit | undefined;
-    private gitFolder: string = '/Users/vinodkiran/code/github/vinodkiran/flowise';
+    private readonly gitFolder: string = '/Users/vinodkiran/code/github/FlowiseAI/FlowiseCloud';
 
     constructor(gitFolder?: string) {
         if (gitFolder) {
@@ -23,27 +24,140 @@ export class FlowiseGitService {
         if (!installed) {
             throw new Error('Git is not installed');
         }
+        this.git.status()
     }
 
-    async getBranches(): Promise<{ branches: string[]; currentBranch: string }> {
+    getRemoteBranches = async(): Promise<{ branches: string[] }> => {
         if (!this.git) {
             throw new Error('Git is not initialized');
         }
         try {
             // Get remote branches
-            const { branches } = await this.git.branch(['-r']);
-            const remoteBranches = Object.keys(branches)
+            const response = await this.git.branch(['-r']);
+            const allBranches = Object.keys(response.branches)
                 .map((name) => name.split('/').slice(1).join('/'))
                 .filter((name) => name !== 'HEAD');
 
-            const { current } = await this.git.branch();
-
             return {
-                branches: remoteBranches,
-                currentBranch: current,
+                branches: allBranches
             };
         } catch (error) {
-            throw new Error('Could not get remote branches from repository', { cause: error });
+            throw new Error('Could not get branches from repository', { cause: error });
+        }
+    }
+
+    public getStatus = async(): Promise<any> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            // Get remote branches
+            return this.git.status()
+        } catch (error) {
+            throw new Error('Could not get status from repository', { cause: error });
+        }
+    }
+
+    public getCurrentBranch = async(): Promise<{ current: string }> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            // Get remote branches
+            const statusResult = await this.git.status()
+            return { current: statusResult.current as string }
+        } catch (error) {
+            throw new Error('Could not get status from repository', { cause: error });
+        }
+    }
+
+    public getTrackingBranch = async(): Promise<{ tracking: string }> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            // Get remote branches
+            const statusResult = await this.git.status()
+            console.log(statusResult)
+            return { tracking: statusResult.current as string }
+        } catch (error) {
+            throw new Error('Could not get status from repository', { cause: error });
+        }
+    }
+
+    public getDirtyFiles = async(): Promise<{ response: DiffResult }> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            const currentBranch = await this.getCurrentBranch()
+            const response = await this.git.diffSummary([currentBranch.current, '--ignore-all-space']);
+            return { response }
+        } catch (error) {
+            throw new Error('Could not get status from repository', { cause: error });
+        }
+    }
+
+    public getLocalBranches = async(): Promise<{ branches: string[] }> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            const response = await this.git.branchLocal()
+            const allBranches = Object.keys(response.branches)
+                .map((name) => name.split('/').slice(1).join('/'))
+                .filter((name) => name !== 'HEAD');
+
+            return {
+                branches: allBranches
+            };
+        } catch (error) {
+            throw new Error('Could not get branches from repository', { cause: error });
+        }
+    }
+
+    public getRemoteURL = async(): Promise<string> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            const response = await this.getConfigValues()
+            if (response) {
+                return response['remote.origin.url'] as string
+            }
+            throw new Error('Could not get remote URL from repository');
+        } catch (error) {
+            throw new Error('Could not get remote URL from repository', { cause: error });
+        }
+    }
+
+    public getUpstreamURL = async(): Promise<string> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            const response = await this.getConfigValues()
+            if (response) {
+                return response['remote.upstream.url'] as string
+            }
+            throw new Error('Could not get upstream URL from repository');
+        } catch (error) {
+            throw new Error('Could not get upstream URL from repository', { cause: error });
+        }
+    }
+
+    public getConfigValues = async(): Promise<ConfigValues> => {
+        if (!this.git) {
+            throw new Error('Git is not initialized');
+        }
+        try {
+            const response = await this.git.listConfig()
+            if (response) {
+                return response.values['.git/config']
+            }
+            throw new Error('Could not get remote URL from repository');
+        } catch (error) {
+            throw new Error('Could not get remote URL from repository', { cause: error });
         }
     }
 
@@ -51,6 +165,7 @@ export class FlowiseGitService {
         if (!this.git) {
             throw new Error('Git is not initialized');
         }
+        console.log('Git Path :: ', this.gitFolder)
         return this.git.version();
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import {FlowiseGitService} from "./FlowiseGitService.js";
 
-const git_folder = '/Users/vinodkiran/code/github/vinodkiran/flowise'
+// const git_folder = '/Users/vinodkiran/code/github/vinodkiran/Flowise'
+const git_folder = '/Users/vinodkiran/code/github/FlowiseAI/FlowiseCloud'
 const flowiseGit = new FlowiseGitService(git_folder)
 await flowiseGit.init()
 
 console.log('Flowise Git Service :: '+ (await flowiseGit.getVersion()))
-const branches = await flowiseGit.getBranches()
-console.log('Current Branch :: '+ branches.currentBranch)
-console.log('Branches :: '+ branches.branches)
-console.log('Happy developing ✨')
+
+const localBranches = await flowiseGit.getLocalBranches()
+console.log('Total Local Branches :: '+ localBranches.branches.length)
+
+const remoteBranches = await flowiseGit.getRemoteBranches()
+console.log('Total Remote Branches :: '+ remoteBranches.branches.length)
+
+const currentBranch = await flowiseGit.getCurrentBranch()
+console.log('Current Branch :: '+ currentBranch.current)
+
+const trackingBranch = await flowiseGit.getTrackingBranch()
+console.log('Tracking Branch :: '+ trackingBranch.tracking)
+
+const remoteUrl = await flowiseGit.getRemoteURL()
+console.log('Remote URL :: '+ remoteUrl)
+
+const upstreamUrl = await flowiseGit.getUpstreamURL()
+console.log('Upstream URL :: '+ upstreamUrl)
+
+const dirtyFiles = await flowiseGit.getDirtyFiles()
+console.log('Dirty Files :: '+ JSON.stringify(dirtyFiles.response, null, 2))
 
